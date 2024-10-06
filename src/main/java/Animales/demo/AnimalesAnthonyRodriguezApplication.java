1a2f9f45a0af549752a58dee2476b964f5b5a09b
anthonyrod07 <144976528+anthonyrod07@users.noreply.github.com>
2024-10-06 11:20:57 -0600

    Se agrega el Proyecto

---
+++ src/main/java/Animales/demo/AnimalesAnthonyRodriguezApplication.java
@@ -0,0 +1,13 @@
+package Animales.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalesAnthonyRodriguezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimalesAnthonyRodriguezApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Animales/demo/AnimalesAnthonyRodriguezApplicationTests.java
@@ -0,0 +1,13 @@
+package Animales.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalesAnthonyRodriguezApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
